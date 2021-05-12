3c883e3c9ac72066a31509b2bf7955e2df264902
Takshak Ramteke <44837950+TakshakRamteke@users.noreply.github.com>
2021-05-13 02:48:10 +0530

    Update pygameinstall.sh

--- pygameinstall.sh
+++ pygameinstall.sh
@@ -1,8 +1,18 @@
 if [ "$(uname -m)" = "x86_64" ]; then
                 sudo pip3 install pygame
         fi
+        
+if [ "$(uname -m)" = "i686" ]; then
+                sudo pip3 install pygame
+        fi
 
 if [ "$(uname -m)" = "aarch64" ]; then
+                apt update
+                apt upgrade -y
+                apt install -y python3-dev gcc python3>
+        fi
+
+if [ "$(uname -m)" = "armv7l" ]; then
                 sudo apt update
                 sudo apt upgrade -y
                 sudo apt install -y python3-dev python3-pygame
